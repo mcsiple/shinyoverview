922ec16e0d15812c24457d94db7bf971f0ba1a9c
Megsie Siple <mcsiple@gmail.com>
2020-12-06 18:52:19 -0800

    add download button

--- 02_dataindataout/explore_ram/server.R
+++ 02_dataindataout/explore_ram/server.R
@@ -12,17 +12,21 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
+    subsetted_data <- reactive({
+        df <- timeseries_values_views %>% 
+            filter_at(vars(TBbest,TCbest, ERbest), any_vars(!is.na(.))) %>%
+            filter(stocklong == input$which_stocklong) 
+        df
+    })
+    
     output$bioparamHead <- renderTable({
-        x <- timeseries_values_views
+        x <- subsetted_data()
         head(x)
     })
     
+    # Time series that the user has selected
     output$ts_plot <- renderPlot({
-        stocks_w_data <- timeseries_values_views %>% 
-            filter_at(vars(TBbest,TCbest, ERbest), any_vars(!is.na(.)))
-        
-        df <- stocks_w_data %>%
-            filter(stocklong == input$which_stocklong) %>%
+        df <- subsetted_data() %>% # Here, we access the "reactive object" subsetted_data
             select(year, input$which_ts) %>%
             pivot_longer(cols = input$which_ts)
         
@@ -30,13 +34,23 @@ shinyServer(function(input, output) {
             ggplot(aes(x = year, y = value, colour = name) ) +
             geom_line(lwd = 1.1) +
             scale_colour_viridis_d("Time series") +
-            labs(title = "Time series selected") +
             xlab("Year") +
             ylab("Value of estimate") +
             facet_wrap(~name) +
-            theme_minimal() +
+            theme_minimal(base_size = 14) +
             theme(legend.position = 'none')
-        
     })
-
+    
+    # Downloadable csv of subsetted data
+    output$summary <- downloadHandler(
+        filename = function() {
+            paste(input$which_stocklong, ".csv", sep = "")
+        },
+        content = function(file) {
+            write.csv(x = subsetted_data(),
+                      file,
+                      row.names = FALSE)
+        }
+    )
+    
 })
--- 02_dataindataout/explore_ram/ui.R
+++ 02_dataindataout/explore_ram/ui.R
@@ -7,39 +7,46 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tidyverse)
 library(shiny)
+library(shinythemes)
 
 load(here::here("02_dataindataout",
                 "explore_ram",
                 "data",
                 "DBdata[asmt][v4.491].RData"))
 
-# Define UI for application that draws a histogram
+# Define UI
 shinyUI(fluidPage(
-
-    # Application title
+    theme = shinytheme("cosmo"), # define theme here
     titlePanel("Explore the RAM legacy database"),
-
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             selectInput(inputId = "which_stocklong",
                         label = "Select a stock:",
-                        choices = unique(timeseries_values_views$stocklong)),
+                        choices = unique(timeseries_values_views$stocklong)
+                        ),
             
             checkboxGroupInput(inputId = "which_ts",
-                        label = "Select a time series to display:",
-                        choiceNames = c("Total biomass",
-                        "Total catch",
-                        "Exploitation rate"),
-                        choiceValues = c("TBbest","TCbest","ERbest")
-                        )
-        ),
-
-        # Show a plot of the generated distribution
+                               label = "Select a time series to display:",
+                               choiceNames = c("Total biomass",
+                                               "Total catch",
+                                               "Exploitation rate"),
+                               choiceValues = c("TBbest", "TCbest", "ERbest"),
+                               selected = "TBbest"
+            ) #/checkboxGroupInput
+        ), # /sidebarPanel
+        
         mainPanel(
+            h2("Your stock from RAM"),
+            plotOutput("ts_plot"),
+            h2("Preview of your RAM estimates"),
             tableOutput("bioparamHead"),
-            plotOutput("ts_plot")
-        )
-    )
+            h2("Download a CSV"),
+            downloadButton(outputId = "summary",
+                           label = "Download CSV")
+        ) # /mainPanel <-- mark the ends of your ui components
+    ) # /sidebarLayout
 ))
