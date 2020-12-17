a3e986111ef6c8939e5d32a6518b967a5a2ad13e
Megsie Siple <mcsiple@gmail.com>
2020-12-17 13:41:14 -0800

    switch to crabs, multiple states

--- 05_ggplotly/00_initializeapp.R
+++ 05_ggplotly/00_initializeapp.R
@@ -1,9 +1,12 @@
 # initialize app
+
+# remotes::install_github("ewenme/ghibli")
 library(tidyverse)
 
 landings <- read.csv(here::here("05_ggplotly","data","nmfslandings.csv"))
 
-ak <- landings %>% 
-  filter(State == "ALASKA") %>%
+crabs <- landings %>% 
+  filter(Confidentiality == "Public") %>%
+  #filter(str_detect(NMFS.Name, "CRAB")) %>%
   mutate_at(c("Pounds","Dollars"), parse_number)
 
